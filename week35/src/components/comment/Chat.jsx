6694b49ee42cd8df0255f813e2f722eba6e302e3
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-04-06 17:00:46 +0300

    chat done

---
+++ week35/src/components/comment/Chat.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import '../comment/Chat.css';
+
+function Chat() {
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  function handleInputChange(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleSendMessage() {
+    if (inputText.trim() === '') {
+      return;
+    }
+
+    const newMessage = {
+      id: Date.now(),
+      text: inputText.trim(),
+      highlighted: true, // добавляем свойство highlighted в новое сообщение
+    };
+
+    setMessages(prevMessages => [newMessage, ...prevMessages]);
+    setInputText('');
+  }
+
+  function handleKeyDown(event) { // обработчик нажатия на клавишу Enter
+    if (event.key === 'Enter') {
+      handleSendMessage();
+    }
+  }
+
+  return (
+    <div className='comment'>
+     <h1>New Chat</h1>
+        {messages.map(message => (
+          <p className='conteiner'
+            key={message.id}
+            style={{ backgroundColor: message.highlighted ? 'lightblue' : '' }}
+          >
+            {message.text}
+          </p>
+        ))}
+      <div>
+        <input className='newComment'
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown} // добавляем обработчик на нажатие клавиши Enter
+          style={{ backgroundColor: 'white' }} // устанавливаем цвет поля ввода
+        />
+        <button className='button' onClick={handleSendMessage}>Send Message</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
